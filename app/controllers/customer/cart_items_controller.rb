ce8c4ed8531eb9362dbb23bb217d18d505958685
EC2 Default User <ec2-user@ip-172-31-7-224.ec2.internal>
2022-04-18 10:44:43 +0000

    注文履歴一覧、注文情報入力、アソシエーション

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,4 +1,23 @@
 class Customer::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items
   end
+  
+  def create
+    
+  end
+  
+  def updated
+    
+  end
+  
+  def destroy
+    CartItem.destroy
+  end
+  
+  def destroy_all
+    CartItem.destroy_all
+  end
+  
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  has_many :order_items, dependent: :destroy
+  belongs_to :genre
+  has_many :cart_items, dependent: :destroy
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
