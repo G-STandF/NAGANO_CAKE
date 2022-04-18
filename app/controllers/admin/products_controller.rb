ccde3f63656b0ce689d45a78202645edc0a9faf7
EC2 Default User <ec2-user@ip-172-31-51-115.ec2.internal>
2022-04-18 08:24:50 +0000

    [Add products/show]

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,10 +7,25 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
+  end
+
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
-  
+
+  # 消費税を加えた商品価格
   def with_tax_price
-    (price * 1.1).floor
+    (self.price * 1.1).round
   end
 end
