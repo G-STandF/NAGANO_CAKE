4c9b839621abef4e2d94a3a126cff414b3fd490a
EC2 Default User <ec2-user@ip-172-31-7-224.ec2.internal>
2022-04-19 03:28:32 +0000

    [Start]adress

---
+++ app/controllers/customer/addresses_controller.rb
@@ -0,0 +1,49 @@
+class Customer::AddressesController < ApplicationController
+  def index
+    @address = Address.new
+    # @addresses = current_customer.addresses
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:notice] = "配送先を登録しました。"
+      redirect_to request.referer
+    else
+      flash.now[:alert] = "配送先を入力してください。"
+      @customer = current_customer
+      @addresses = @customer.addresses
+      render 'index'
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "配送先を更新しました。"
+      redirect_to addresses_path
+    else
+      flash.now[:alert] = "配送先を入力してください。"
+      render "edit"
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    flash[:notice] = "配送先を削除しました。"
+    redirect_to request.referer
+  end
+
+  
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+end
