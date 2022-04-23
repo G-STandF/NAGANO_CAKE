f6e853cf1837a23310f0f26e8dc8e1b3fc9d89cc
EC2 Default User <ec2-user@ip-172-31-51-115.ec2.internal>
2022-04-23 11:23:42 +0000

    [error 非同期通信化]

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -27,7 +27,6 @@ class Customer::CartItemsController < ApplicationController
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
-    redirect_to cart_items_path
   end
 
   def destroy
