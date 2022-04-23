77fd45df2a42ac59c2352059a54138ea7d3ee0b7
EC2 Default User <ec2-user@ip-172-31-51-115.ec2.internal>
2022-04-23 07:21:34 +0000

    [?]

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -18,7 +18,7 @@ class Customer::ProductsController < ApplicationController
   end
 
   private
-  
+
   def cart_item_params
     params.require(:cart_items).permit(:product_id, :product_count)
   end
